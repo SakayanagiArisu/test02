3de790c2d9cf3fb18722d39413e7f9fd8e7eeb0d
Artur <arturro.solarro@gmail.com>
2020-12-06 12:46:07 +0100

    commit servara

--- server.js
+++ server.js
@@ -8,8 +8,65 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('static'));
 
+var users = [
+    {log: 'aaa', pass: 'qwerty', uczen: 'checked', plec: 'm', id: 1, wiek: 18},
+    {log: 'bbb', pass: '1234', uczen: 'checked', plec: 'k', id: 2, wiek: 12},
+    {log: 'miki2008', pass: 'super', uczen: '', plec: 'm', id: 3, wiek: 14},
+    {log: 'ania', pass: 'ania', uczen: '', plec: 'k', id: 4, wiek: 13}
+]
+
+var active = null;
+
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/static/index.html'));
+})
+
+app.get("/register", function (req, res) {
+    res.sendFile(path.join(__dirname + "/static/register.html"));
+})
+
+app.post("/register", function(req, res){
+    let test = false;
+    for(let i = 0; i < users.length; i++){
+        if(users[i]['log'] == req.body.login){
+            test = true;
+            break;
+        }
+    }
+    if(test){
+        res.send('Już istnieje użytkownik o takim loginie')
+    }else{
+        let x = req.body;
+        let id = users.length + 1;
+        let student;
+        if(x.student == undefined){
+            student = '';
+        }else{
+            student = 'checked';
+        }
+        users.push({log: x.login, pass: x.password, uczen: student, plec: x.gender, id: id, wiek: parseInt(x.age)});
+        res.send('Zarejestrowano pomyślnie');
+    }
+})
+
+app.get("/login", function (req, res) {
+    res.sendFile(path.join(__dirname + "/static/login.html"));
+})
+
+app.post("/login", function(req, res){
+    let test = true;
+    it = null;
+    for(let i = 0; i < users.length; i++){
+        if(users[i]['log'] == req.body.login && users[i]['pass'] == req.body.password){
+            test = false;
+            active = users[i]['log'];
+            res.send('Zalogowano pomyślnie')
+            break;
+        }
+    }
+    if(test){
+        res.send('Nie ma użytkownika o takim loginie i haśle');
+    }
 })
 
 app.listen(PORT, function () {
