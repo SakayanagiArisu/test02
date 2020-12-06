f0d8ed5b20589a4d08b81a2dfe8f23ad6120eba8
Artur <arturro.solarro@gmail.com>
2020-12-06 12:44:35 +0100

    commit servera

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 3000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
 
-//var bodyParser = require("body-parser");
+var bodyParser = require("body-parser");
 
-//app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 app.use(express.static('static'));
 
