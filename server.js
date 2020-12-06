ac6a1e8726bcdf224947e55b43a4e27c3914a194
Artur <arturro.solarro@gmail.com>
2020-12-06 12:57:53 +0100

    commit czegos

--- server.js
+++ server.js
@@ -27,8 +27,48 @@ app.get("/register", function (req, res) {
     res.sendFile(path.join(__dirname + "/static/register.html"));
 })
 
+app.post("/register", function(req, res){
+    let test = false;
+    for(let i = 0; i < users.length; i++){
+        if(users[i]['log'] == req.body.login){
+            test = true;
+            break;
+        }
+    }
+    if(test){
+        res.send('Już istnieje użytkownik o takim loginie')
+    }else{
+        let x = req.body;
+        let id = users.length + 1;
+        let student;
+        if(x.student == undefined){
+            student = '';
+        }else{
+            student = 'checked';
+        }
+        users.push({log: x.login, pass: x.password, uczen: student, plec: x.gender, id: id, wiek: parseInt(x.age)});
+        res.send('Zarejestrowano pomyślnie');
+    }
+})
+
 app.get("/login", function (req, res) {
     res.sendFile(path.join(__dirname + "/static/login.html"));
+})
+
+app.post("/login", function(req, res){
+    let test = true;
+    it = null;
+    for(let i = 0; i < users.length; i++){
+        if(users[i]['log'] == req.body.login && users[i]['pass'] == req.body.password){
+            test = false;
+            active = users[i]['log'];
+            res.send('Zalogowano pomyślnie')
+            break;
+        }
+    }
+    if(test){
+        res.send('Nie ma użytkownika o takim loginie i haśle');
+    }
 })
 
 app.get("/admin", function (req, res) {
